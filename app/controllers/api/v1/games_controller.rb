2a62a439999d847a57d75f14b2e7fe2ba8dbf823
Daniel <daniel@Alexs-MacBook-Air.local>
2021-11-29 19:17:08 -0500

    added show page for games

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -16,8 +16,27 @@ class Api::V1::GamesController < ApplicationController
   
     response = http.request(request)
     data = response.read_body
-   
+    
 
   render json: { data: data }
   end
+  def show
+    id = ""
+    url = URI("https://free-to-play-games-database.p.rapidapi.com/api/game?id=#{params["id"]}")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    
+    request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-host"] = 'free-to-play-games-database.p.rapidapi.com'
+    request["x-rapidapi-key"] = 'aec551c9b7msh5a61fd88e0d4ccbp1d8828jsn959d761e2a14'
+    
+    response = http.request(request)
+    data = response.read_body
+
+    render json: { data: data }
+
+    
+end
 end
--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import GamesIndexPage from "./games/GamesIndexPage"
+import GamesShow from "./games/GamesShow"
 export const App = (props) => {
   return (
     <BrowserRouter>
     <Switch>
       <Route exact path ="/" component={GamesIndexPage} />
       <Route exact path ="/games" component ={GamesIndexPage} />
+      <Route exact path ="/games/:id" component={GamesShow} />
     </Switch>
     </BrowserRouter>
   )
