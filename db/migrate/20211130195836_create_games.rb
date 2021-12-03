54d52f2b4b79ccc156bf96240d763ca55b622f26
Daniel <daniel@Alexs-Air.lan1>
2021-12-02 22:58:38 -0500

    added new collums for all API data

--- db/migrate/20211130195836_create_games.rb
+++ db/migrate/20211130195836_create_games.rb
@@ -4,6 +4,15 @@ class CreateGames < ActiveRecord::Migration[6.1]
       t.string :title, null: false
       t.string :thumbnail
       t.string :genre
+      t.text :short_description
+      t.text :description
+      t.text :game_url
+      t.text :platform
+      t.text :publisher
+      t.text :developer
+      t.text :release_date
+      t.text :minimum_system_requirements
+      t.text :screenshots
       t.string :api_id
       t.timestamps
     end
