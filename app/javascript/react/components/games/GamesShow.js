9c93871bde0b16d979993cdc8451e6db75f7cadb
Daniel <danielzhdanov31@gmail.com>
2022-01-21 04:45:54 -0500

    added backround style to show page

--- app/javascript/react/components/games/GamesShow.js
+++ app/javascript/react/components/games/GamesShow.js
@@ -114,130 +114,134 @@ const GamesShow = (props) => {
 	} else {
 		text = game.description;
 	}
-
+	let backgroundImage;
 	return (
 		<div className='q1'>
-			<div className='left'>
-				{" "}
-				<img src={game.thumbnail} />{" "}
-				<div>
-					<h2 className='play-now'>
-						<a className='play-now' href={game.game_url} target='_blank'>
-							{" "}
-							<strong>Play Now</strong>{" "}
-						</a>
-					</h2>
-					{/* <h4>Favorite</h4> */}
+			<div className='gameprofile_backround'>
+				<div className='gameprofile_gradient'>
+					<div className='left'>
+						{" "}
+						<img src={game.thumbnail} />{" "}
+						<div>
+							<h2 className='play-now'>
+								<a className='play-now' href={game.game_url} target='_blank'>
+									{" "}
+									<strong>Play Now</strong>{" "}
+								</a>
+							</h2>
+							{/* <h4>Favorite</h4> */}
+						</div>
+					</div>
+					<div className='right'>
+						{" "}
+						<h1>{game.title}</h1>
+						<p> {reviewNumber} Comments</p>
+						{/* <p>3 Favorite</p> */}
+						<p>
+							{text}{" "}
+							<span onClick={toggleShowMore}>
+								{showMoreStatus ? "Read More" : "Read Less"}
+							</span>
+						</p>
+						<div className='info'>
+							<h2>Aditional information</h2>
+						</div>
+						<div className='row-flex'>
+							<div className='col-6 col-md-4'>
+								<span>
+									<strong>Title</strong>
+								</span>
+								<p>{game.title}</p>
+							</div>
+							<div className='col-6 col-md-4'>
+								<span>
+									<strong>Developer</strong>
+								</span>
+								<p>{game.developer}</p>
+							</div>
+							<div className='col-6 col-md-4'>
+								<span>
+									<strong>Publisher</strong>
+								</span>
+								<p>{game.publisher}</p>
+							</div>
+							<div className='col-6 col-md-4'>
+								<span>
+									<strong>Release Date</strong>
+								</span>
+								<p>{game.release_date}</p>
+							</div>
+							<div className='col-6 col-md-4'>
+								<span>
+									<strong>Genre</strong>
+								</span>
+								<p>{game.genre}</p>
+							</div>
+							<div className='col-6 col-md-4'>
+								<span>
+									<strong>Platform</strong>
+								</span>
+								<p>{game.platform}</p>
+							</div>
+						</div>
+						<h2>{game.title} Screenshots</h2>
+						<div className='row-flex'>
+							<div className='col-6 col-md-4'>
+								<img className='image' src={game.screenshot1} alt='hey'></img>
+							</div>
+							<div className='col-6 col-md-4'>
+								<img className='image' src={game.screenshot2} alt='hey'></img>
+							</div>
+							<div className='col-6 col-md-4'>
+								<img className='image' src={game.screenshot3} alt='hey'></img>
+							</div>
+							<div className='info'>
+								<h2>Minimum System Requirements</h2>
+							</div>
+							<div className='row-flex'>
+								<div className='col-6 col-md-4'>
+									<span>
+										<strong>OS</strong>
+									</span>
+									<p>{game.os1}</p>
+								</div>
+								<div className='col-6 col-md-4'>
+									<span>
+										<strong>Processor</strong>
+									</span>
+									<p>{game.os2}</p>
+								</div>
+								<div className='col-6 col-md-4'>
+									<span>
+										<strong>Memory</strong>
+									</span>
+									<p>{game.os3}</p>
+								</div>
+								<div className='col-6 col-md-4'>
+									<span>
+										<strong>Graphics</strong>
+									</span>
+									<p>{game.os4}</p>
+								</div>
+								<div className='col-6 col-md-4'>
+									<span>
+										<strong>Storage</strong>
+									</span>
+									<p>{game.os5}</p>
+								</div>
+								<div className='col-6 col-md-4'>
+									<span>
+										<strong>Aditional Notes</strong>
+									</span>
+									<p>Specifications may change during development</p>
+								</div>
+							</div>
+						</div>
+						<div className='form-line'>{createReviews}</div>
+						<h2>User Comments</h2>
+						<div>{reviewTiles}</div>
+					</div>
 				</div>
-			</div>
-			<div className='right'>
-				{" "}
-				<h1>{game.title}</h1>
-				<p> {reviewNumber} Comments</p>
-				{/* <p>3 Favorite</p> */}
-				<p>
-					{text}{" "}
-					<span onClick={toggleShowMore}>
-						{showMoreStatus ? "Read More" : "Read Less"}
-					</span>
-				</p>
-				<div className='info'>
-					<h2>Aditional information</h2>
-				</div>
-				<div className='row-flex'>
-					<div className='col-6 col-md-4'>
-						<span>
-							<strong>Title</strong>
-						</span>
-						<p>{game.title}</p>
-					</div>
-					<div className='col-6 col-md-4'>
-						<span>
-							<strong>Developer</strong>
-						</span>
-						<p>{game.developer}</p>
-					</div>
-					<div className='col-6 col-md-4'>
-						<span>
-							<strong>Publisher</strong>
-						</span>
-						<p>{game.publisher}</p>
-					</div>
-					<div className='col-6 col-md-4'>
-						<span>
-							<strong>Release Date</strong>
-						</span>
-						<p>{game.release_date}</p>
-					</div>
-					<div className='col-6 col-md-4'>
-						<span>
-							<strong>Genre</strong>
-						</span>
-						<p>{game.genre}</p>
-					</div>
-					<div className='col-6 col-md-4'>
-						<span>
-							<strong>Platform</strong>
-						</span>
-						<p>{game.platform}</p>
-					</div>
-				</div>
-				<h2>{game.title} Screenshots</h2>
-				<div className='row-flex'>
-					<div className='col-6 col-md-4'>
-						<img className='image' src={game.screenshot1} alt='hey'></img>
-					</div>
-					<div className='col-6 col-md-4'>
-						<img className='image' src={game.screenshot2} alt='hey'></img>
-					</div>
-					<div className='col-6 col-md-4'>
-						<img className='image' src={game.screenshot3} alt='hey'></img>
-					</div>
-					<div className='info'>
-						<h2>Minimum System Requirements</h2>
-					</div>
-					<div className='row-flex'>
-						<div className='col-6 col-md-4'>
-							<span>
-								<strong>OS</strong>
-							</span>
-							<p>{game.os1}</p>
-						</div>
-						<div className='col-6 col-md-4'>
-							<span>
-								<strong>Processor</strong>
-							</span>
-							<p>{game.os2}</p>
-						</div>
-						<div className='col-6 col-md-4'>
-							<span>
-								<strong>Memory</strong>
-							</span>
-							<p>{game.os3}</p>
-						</div>
-						<div className='col-6 col-md-4'>
-							<span>
-								<strong>Graphics</strong>
-							</span>
-							<p>{game.os4}</p>
-						</div>
-						<div className='col-6 col-md-4'>
-							<span>
-								<strong>Storage</strong>
-							</span>
-							<p>{game.os5}</p>
-						</div>
-						<div className='col-6 col-md-4'>
-							<span>
-								<strong>Aditional Notes</strong>
-							</span>
-							<p>Specifications may change during development</p>
-						</div>
-					</div>
-				</div>
-				<div className='form-line'>{createReviews}</div>
-				<h2>User Comments</h2>
-				<div>{reviewTiles}</div>
 			</div>
 		</div>
 	);
